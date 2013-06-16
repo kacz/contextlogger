a029acf992a07514c94eb7115e2baa0935c8d6d0
kacz <kaczkrisztian@gmail.com>
2013-06-16 18:43:37 +0200

    * TrafficListener: new values logged.

--- contextlogger/src/cz/cuni/kacz/contextlogger/listeners/TrafficListener.java
+++ contextlogger/src/cz/cuni/kacz/contextlogger/listeners/TrafficListener.java
@@ -23,13 +23,9 @@ package cz.cuni.kacz.contextlogger.listeners;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import android.annotation.SuppressLint;
 import android.net.TrafficStats;
 import android.os.Process;
-import android.provider.Settings.SettingNotFoundException;
-import android.util.Log;
-
-import cz.cuni.kacz.contextlogger.ContextLogger;
-import cz.cuni.kacz.contextlogger.ContextLoggerService;
 import cz.cuni.kacz.contextlogger.DataManager;
 import cz.cuni.kacz.contextlogger.TimeSource;
 
@@ -40,25 +36,37 @@ public class TrafficListener extends DefaultContextListener {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private boolean running = false;
+	private final int API_VERSION = android.os.Build.VERSION.SDK_INT;
+	private final boolean running = false;
 	private Timer timer;
 	int period = 100;
 
 	// log names and types
-	private String labelTotalSent = "Total sent traffic";
-	private int typeTotalSent = DataManager.LONG;
-	private String labelTotalRcvd = "Total received traffic";
-	private int typeTotalRcvd = DataManager.LONG;
-	private String labelMySent = "My sent traffic";
-	private int typeMySent = DataManager.LONG;
-	private String labelMyRcvd = "My received traffic";
-	private int typeMyRcvd = DataManager.LONG;
+	private final String labelTotalSent = "Total sent traffic";
+	private final int typeTotalSent = DataManager.LONG;
+	private final String labelTotalRcvd = "Total received traffic";
+	private final int typeTotalRcvd = DataManager.LONG;
+	private final String labelMySent = "My sent traffic";
+	private final int typeMySent = DataManager.LONG;
+	private final String labelMyRcvd = "My received traffic";
+	private final int typeMyRcvd = DataManager.LONG;
+
+	private final String labelTotalSentPackets = "Total sent packets";
+	private final int typeTotalSentPackets = DataManager.LONG;
+	private final String labelTotalRcvdPackets = "Total received packets";
+	private final int typeTotalRcvdPackets = DataManager.LONG;
+	private final String labelMySentPackets = "My sent packets";
+	private final int typeMySentPackets = DataManager.LONG;
+	private final String labelMyRcvdPackets = "My received packets";
+	private final int typeMyRcvdPackets = DataManager.LONG;
 
 	@Override
 	public void startListening() {
 		final int myUid = Process.myUid();
 		timer = new Timer();
 		timer.scheduleAtFixedRate(new TimerTask() {
+			@SuppressLint("NewApi")
+			@Override
 			public void run() {
 				long time = TimeSource.getTimeOfDay();
 				mDataManager.insertLog(labelTotalSent, time,
@@ -69,6 +77,18 @@ public class TrafficListener extends DefaultContextListener {
 						TrafficStats.getUidTxBytes(myUid));
 				mDataManager.insertLog(labelMyRcvd, time,
 						TrafficStats.getUidRxBytes(myUid));
+
+				mDataManager.insertLog(labelTotalSentPackets, time,
+						TrafficStats.getTotalTxPackets());
+				mDataManager.insertLog(labelTotalRcvdPackets, time,
+						TrafficStats.getTotalRxPackets());
+				if (API_VERSION >= android.os.Build.VERSION_CODES.HONEYCOMB_MR1) {
+					mDataManager.insertLog(labelMySentPackets, time,
+							TrafficStats.getUidTxPackets(myUid));
+					mDataManager.insertLog(labelMyRcvdPackets, time,
+							TrafficStats.getUidRxPackets(myUid));
+				}
+
 			}
 		}, 0, period);
 	}
@@ -84,6 +104,13 @@ public class TrafficListener extends DefaultContextListener {
 		addLogType(labelTotalRcvd, typeTotalRcvd);
 		addLogType(labelMySent, typeMySent);
 		addLogType(labelMyRcvd, typeMyRcvd);
+
+		addLogType(labelTotalSentPackets, typeTotalSentPackets);
+		addLogType(labelTotalRcvdPackets, typeTotalRcvdPackets);
+		if (API_VERSION >= android.os.Build.VERSION_CODES.HONEYCOMB_MR1) {
+			addLogType(labelMySentPackets, typeMySentPackets);
+			addLogType(labelMyRcvdPackets, typeMyRcvdPackets);
+		}
 	}
 
 }
